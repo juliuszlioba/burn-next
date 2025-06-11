02bd306753183ac35eb2df98be412731323e0334
juliuszlioba <julius.zlioba@gmail.com>
2025-06-11 16:11:24 +0200

    v1.0.7

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
 
   if (!data || data.length === 0) {
     return (
-      <main className="flex flex-col items-center p-4 space-y-4">
+      <main className="flex flex-col items-center p-4 space-y-4 max-w-lg w-full mx-auto">
         <Card className="w-full text-center">
           <CardContent>
             <div className="text-2xl">No records</div>
@@ -37,7 +37,7 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex flex-col items-center p-4 space-y-4 max-w-lg mx-auto">
+    <main className="flex flex-col items-center p-4 space-y-4 max-w-lg w-full mx-auto">
       <Card className="w-full text-center bg-gradient-to-r from-red-500 to-orange-400 p-0.5">
         <CardContent className="bg-background rounded-sm p-4">
           <div className="text-2xl font-semibold bg-gradient-to-r from-red-500 to-orange-400 bg-clip-text text-transparent">
